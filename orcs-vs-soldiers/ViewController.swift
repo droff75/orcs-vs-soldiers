449d95b432266d33521cf04d31ff75aa57477eda
david.roff <david.roff@asynchrony.com>
2016-05-11 12:28:01 -0500

    Initial Commit

---
+++ orcs-vs-soldiers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  orcs-vs-soldiers
+//
+//  Created by dev1 on 5/11/16.
+//  Copyright © 2016 dev1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
